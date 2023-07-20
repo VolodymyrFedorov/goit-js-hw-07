87c782470630937ae59079005a6c787a000a1243
VolodymyrFedorov <133118295+VolodymyrFedorov@users.noreply.github.com>
2023-07-20 21:16:45 +0300

    2-nd task

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,39 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector('.gallery');
+
+const galleryMarkup = galleryItems
+  .map(item => `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${item.original}">
+        <img
+          class="gallery__image"
+          src="${item.preview}"
+          data-source="${item.original}"
+          alt="${item.description}"
+        />
+      </a>
+    </li>
+  `)
+  .join('');
+
+gallery.innerHTML = galleryMarkup;
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt', 
+  captionPosition: 'bottom',
+});
+
+lightbox.on('show.simplelightbox', () => {
+  setTimeout(() => {
+    const captionElements = document.querySelectorAll('.gallery__caption');
+    captionElements.forEach((caption) => caption.classList.add('visible'));
+  }, 250);
+});
+
+lightbox.on('close.simplelightbox', () => {
+  const captionElements = document.querySelectorAll('.gallery__caption');
+  captionElements.forEach((caption) => caption.classList.remove('visible'));
+});
 
 console.log(galleryItems);
